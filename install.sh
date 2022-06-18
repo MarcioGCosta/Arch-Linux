b398984cedf15376e370ff290fd5c3af004a8587
MarcioGCosta <marcio.gustavo.2006@gmail.com>
2022-06-18 20:50:38 -0300

    dash as bin sh

--- install.sh
+++ install.sh
@@ -28,6 +28,10 @@ mv $HOME/Arch-Linux/config/.xinitrc /$HOME/.xinitrc && mv $HOME/Arch-Linux/confi
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+
+## Symlinking dash as /bin/sh
+sudo ln -sfT dash /usr/bin/sh
+
 ## Installing yay
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd .. && rm -rf yay
 
