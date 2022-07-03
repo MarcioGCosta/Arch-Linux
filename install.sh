a0651ff790428f1f072c8f1a521ef0eac3ff642d
MarcioGCosta <marcio.gustavo.2006@gmail.com>
2022-07-02 21:15:02 -0300

    hello

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ yes | sudo pacman -Sy reflector && sudo reflector --sort score --threads 5 --sav
 sudo mv $HOME/Arch-Linux/config/20-intel-gpu.conf /etc/X11/xorg.conf.d/
 
 ## Installing misc progams
-yes | sudo pacman -Syu xf86-video-intel xorg-server xorg-setxkbmap xorg-xrandr pulsemixer qbittorrent bspwm sxhkd arc-gtk-theme bat ripgrep htop ufw ntp wireplumber pipewire-jack firefox polybar kitty mpv yt-dlp pipewire papirus-icon-theme pipewire-pulse pipewire-alsa libva-intel-driver dmenu playerctl dash go zsh noto-fonts-emoji ttf-nerd-fonts-symbols ttf-ibm-plex feh picom neovim doas yarn npm xsel
+yes | sudo pacman -Syu libvdpau-va-gl xf86-video-intel xorg-server xorg-setxkbmap xorg-xrandr pulsemixer qbittorrent bspwm sxhkd arc-gtk-theme bat ripgrep htop ufw ntp wireplumber pipewire-jack firefox polybar kitty mpv yt-dlp pipewire papirus-icon-theme pipewire-pulse pipewire-alsa libva-intel-driver dmenu playerctl dash go zsh noto-fonts-emoji ttf-nerd-fonts-symbols ttf-ibm-plex feh picom neovim doas yarn npm xsel
 
 ## Moving folders
 mv $HOME/Arch-Linux/config/.zshrc /$HOME/.zshrc && mv $HOME/Arch-Linux/config/.config /$HOME/ && mv $HOME/Arch-Linux/background /$HOME/.config/ && mv $HOME/Arch-Linux/config/.gtkrc-2.0 ..
